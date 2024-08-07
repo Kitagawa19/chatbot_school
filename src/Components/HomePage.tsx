44b368f22826c7c5d9f30632bef440c7d5be4e99
Kitagawa19 <kkitagawa16219@gmail.com>
2024-08-07 14:32:55 +0900

    add chat content & add authinfo

---
+++ src/Components/HomePage.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useAuth } from '../Context/authContext';
+import LoginForm from './LoginForm';
+import ChatComponent from './ChatComponent';
+
+const HomePage: React.FC = () => {
+    const { user } = useAuth();
+
+    return (
+        <div>
+            <h1>Welcome to Our Chat App</h1>
+            {user ? (
+                <>
+                    <p>Hello, {user.username}!</p>
+                    <ChatComponent />
+                </>
+            ) : (
+                <>
+                    <p>Please log in to start chatting.</p>
+                    <LoginForm />
+                </>
+            )}
+        </div>
+    );
+};
+
+export default HomePage;
