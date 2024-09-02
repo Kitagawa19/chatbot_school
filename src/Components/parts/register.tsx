2f50c669bf0b8cd4fff9b80faaa068ec80521581
Kitagawa19 <104189576+Kitagawa19@users.noreply.github.com>
2024-09-02 14:00:35 +0900

    create register   fucntion

--- src/Components/parts/register.tsx
+++ src/Components/parts/register.tsx
@@ -1,2 +1,29 @@
-import React from 'react';
-export const register= ()=>{}
+'use client';
+import React, { useState, FormEvent } from 'react';
+import { Button, Container, Grid, TextField, Typography, InputAdornment, IconButton } from '@mui/material';
+
+export const RegisterForm: React.FC = () => {
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    const handleMouseDownPassword = (e: React.MouseEvent<HTMLButtonElement>) => e.preventDefault();
+    const async handlesubmit = () => {
+      const res =  await fetch('',{
+        headers:{},
+        bodys:{
+          username:name,
+          email:email,
+          password:password,
+        }
+      });
+      if (!res){
+        return(
+          Console.log('response not found')
+        )
+      }
+    }
+};
