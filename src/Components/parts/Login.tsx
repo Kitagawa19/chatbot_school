6580412745ebdf6a9ac651f24949665248a6c7f1
KitagawaKouta <kkitagawa16219@gmail.com>
2024-10-05 15:26:00 +0900

    update カード化していたコンポーネントを最小化

--- src/Components/parts/Login.tsx
+++ src/Components/parts/Login.tsx
@@ -1,133 +1,121 @@
 'use client';
 import React, { useState, FormEvent } from 'react';
 import { useAuth } from '../../Context/authContext';
-import { 
-  Button, Container, Grid, TextField, Typography, InputAdornment, 
+import {
+  Button, Container, Grid, TextField, Typography, InputAdornment,
   IconButton, Box, Link, Snackbar, Alert
 } from '@mui/material';
 import { Visibility, VisibilityOff, Email, Lock } from '@mui/icons-material';
 
 export const LoginForm: React.FC = () => {
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [showPassword, setShowPassword] = useState<boolean>(false);
-    const [showSnackbar, setShowSnackbar] = useState<boolean>(false);
-    const [snackbarMessage, setSnackbarMessage] = useState<string>('');  
-    const [snackbarSeverity, setSnackbarSeverity] = useState<'success' | 'error'>('success');  
-    const { login, error } = useAuth();  // エラーとlogin関数を取得
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showSnackbar, setShowSnackbar] = useState<boolean>(false);
+  const [snackbarMessage, setSnackbarMessage] = useState<string>('');
+  const [snackbarSeverity, setSnackbarSeverity] = useState<'success' | 'error'>('success');
+  const { login, error } = useAuth();  // エラーとlogin関数を取得
 
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-    const handleMouseDownPassword = (e: React.MouseEvent<HTMLButtonElement>) => e.preventDefault();
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (e: React.MouseEvent<HTMLButtonElement>) => e.preventDefault();
 
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log("Form submitted, calling login function...");
-        try {
-            await login(email, password);
-            setSnackbarMessage('ログインに成功しました');  
-            setSnackbarSeverity('success');
-        } catch (err) {
-            setSnackbarMessage('ログインに失敗しました');  
-            setSnackbarSeverity('error');
-        } finally {
-            setShowSnackbar(true);  
-        }
-    };
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted, calling login function...");
+    try {
+      await login(email, password);
+      setSnackbarMessage('ログインに成功しました');
+      setSnackbarSeverity('success');
+    } catch (err) {
+      setSnackbarMessage('ログインに失敗しました');
+      setSnackbarSeverity('error');
+    } finally {
+      setShowSnackbar(true);
+    }
+  };
 
-    const handleSnackbarClose = () => {
-        setShowSnackbar(false);
-    };
+  const handleSnackbarClose = () => {
+    setShowSnackbar(false);
+  };
 
-    return (
-        <Box sx={{ mt: 8, mb: 4 }}>
-            <Container maxWidth="sm">
-                <Typography variant="h4" component="h1" align="center" gutterBottom>
-                    ログイン
-                </Typography>
-                <form onSubmit={handleSubmit}>
-                    <Grid container spacing={3}>
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                label="メールアドレス"
-                                variant="outlined"
-                                type="email"
-                                value={email}
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-                                required
-                                InputProps={{
-                                    startAdornment: (
-                                        <InputAdornment position="start">
-                                            <Email />
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                label="パスワード"
-                                variant="outlined"
-                                type={showPassword ? 'text' : 'password'}
-                                value={password}
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-                                required
-                                InputProps={{
-                                    startAdornment: (
-                                        <InputAdornment position="start">
-                                            <Lock />
-                                        </InputAdornment>
-                                    ),
-                                    endAdornment: (
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                aria-label="toggle password visibility"
-                                                onClick={handleClickShowPassword}
-                                                onMouseDown={handleMouseDownPassword}
-                                                edge="end"
-                                            >
-                                                {showPassword ? <Visibility /> : <VisibilityOff />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    )
-                                }}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                color="primary"
-                                size="large"
-                            >
-                                ログイン
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </form>
-                <Box mt={3} textAlign='center'>
-                    <Typography color='primary'>
-                        <Link href='/Register' underline='hover'>
-                            アカウントをお持ちではないですか？登録はこちら
-                        </Link>
-                    </Typography>
-                </Box>
-            </Container>
-            <Snackbar
-                open={showSnackbar}
-                autoHideDuration={3000}
-                onClose={handleSnackbarClose}
+  return (
+    <Box sx={{ mt: 8, mb: 4 }}>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="メールアドレス"
+              variant="outlined"
+              type="email"
+              value={email}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+              required
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Email />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="パスワード"
+              variant="outlined"
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+              required
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Lock />
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              size="large"
             >
-                <Alert
-                    onClose={handleSnackbarClose}
-                    severity={snackbarSeverity}
-                    sx={{ width: '100%' }}
-                >
-                    {snackbarMessage}  
-                </Alert>
-            </Snackbar>
-        </Box>
-    );
+              ログイン
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+      <Snackbar
+        open={showSnackbar}
+        autoHideDuration={3000}
+        onClose={handleSnackbarClose}
+      >
+        <Alert
+          onClose={handleSnackbarClose}
+          severity={snackbarSeverity}
+          sx={{ width: '100%' }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+    </Box>
+  );
 };
